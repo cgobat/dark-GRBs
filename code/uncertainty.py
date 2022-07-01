f2a24e3e7e6df603ba6b05336b730e3b0ac81ad0
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2022-07-01 12:16:44 -0600

    Array handling and functional improvements

--- code/uncertainty.py
+++ code/uncertainty.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def pos_errors(uncertainty_array):
-    return [v.plus for v in uncertainty_array]
+def pos_errors(array):
+    return [v.plus for v in array]
 
-def neg_errors(uncertainty_array):
-    return [v.minus for v in uncertainty_array]
+def neg_errors(array):
+    return [v.minus for v in array]
 
 class AsymmetricUncertainty:
     """
@@ -59,7 +59,7 @@ class AsymmetricUncertainty:
             self.minus = np.abs(float(neg_err))
         self.maximum = self.value+self.plus
         self.minimum = self.value-self.minus
-        self.sign = self.value/np.abs(self.value) if self.value != 0 else 1
+        self.sign = 1 if self.value >= 0 else -1
         self.is_symmetric = np.isclose(self.plus, self.minus)
         
     def __str__(self):
@@ -108,11 +108,11 @@ class AsymmetricUncertainty:
             new_pos = np.sqrt(self.plus**2 + delta**2)
             new_neg = np.sqrt(self.minus**2 + delta**2)
         elif method=="straight":
-            new_pos += delta
-            new_neg -= delta
+            new_pos = self.plus + delta
+            new_neg = self.minus + delta
         elif method=="split":
-            new_pos += delta/2
-            new_neg -= delta/2
+            new_pos = self.plus + delta/2
+            new_neg = self.minus + delta/2
         else:
             raise ValueError
         if inplace:
@@ -326,14 +326,16 @@ class UncertaintyArray(list):
     
     def refresh(self):
         for i in range(len(self)):
-            if isinstance(self[i], AsymmetricUncertainty):
-                pass
-            else:
+            try:
+                self[i].value
+                self[i].plus
+                self[i].minus
+            except AttributeError:
                 self[i] = AsymmetricUncertainty(self[i],0,0)
                 
         self.as_numpy = np.array(self.as_list)
         self.flattened = self.as_numpy.flatten()
-        self.shape = np.shape(self.as_numpy)
+        self.shape = self.as_numpy.shape
         self.ndim = self.as_numpy.ndim
 
         self.minus = [v.minus for v in self.as_list]
@@ -344,15 +346,21 @@ class UncertaintyArray(list):
 
         self.as_list = list(array)
         self.refresh()
-                        
+
     def __len__(self):
         return len(self.as_list)
+
+    def __iter__(self):
+        return iter(self.as_list)
 
     def __getitem__(self, key):
         return self.as_list[key]
 
+    def __setitem__(self, key, val):
+        self.as_list[key] = val
+
     def __str__(self):
-        return str(self.as_list)
+        return str([str(entry) for entry in self.as_list])
 
     def __repr__(self):
         return str(self)
@@ -363,3 +371,6 @@ class UncertaintyArray(list):
     def append(self,entry):
         self.as_list.append(entry)
         self.refresh()
+    
+    def pdf(self,x):
+        return np.sum([entry.pdf(x) for entry in self], axis=0)
